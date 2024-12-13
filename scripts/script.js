217a4d4e6371efffa5879a206c2c31f49b1f4326
Astiazj <145377464+Astiazj@users.noreply.github.com>
2024-12-13 18:14:58 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,40 @@
 // JavaScript Document
 console.log("hi");
 
-const menuButton = document.querySelector("nav > ul > li:nth-of-type(3) > nav > button");
+const menuButton = document.querySelector(
+  "nav > ul > li:nth-of-type(3) > nav > button"
+);
 const deNav = document.querySelector("nav > ul > li:nth-of-type(3) > nav");
-const zoekButton = document.querySelector("nav > ul:nth-of-type(1) > li:nth-of-type(2) > nav > button");
-const zoekNav = document.querySelector("nav > ul:nth-of-type(1) > li:nth-of-type(2) > nav");
-const sluitZoek = document.querySelector("nav > ul:nth-of-type(1) > li:nth-of-type(2) > nav > section > button");
-const winkelButton = document.querySelector("nav > ul:nth-of-type(1) > li:nth-of-type(1) > section > button:nth-of-type(1)");
-const sluitWinkel = document.querySelector("nav > ul:nth-of-type(1) > li:nth-of-type(1) > section > section > button > img");
-const winkelNav = document.querySelector("nav > ul:nth-of-type(1) > li:nth-of-type(1) > section");
-const rechtsButton = document.querySelector("main > section:nth-of-type(2) > section > button:nth-of-type(2)");
-const linksButton = document.querySelector("main > section:nth-of-type(2) > section > button:nth-of-type(1)");
-const artikelList = document.querySelector("main > section:nth-of-type(2) > section > ul");
-const hamburgerButton = document.querySelector("nav > ul > li:nth-of-type(3) > nav > button");
+const zoekButton = document.querySelector(
+  "nav > ul:nth-of-type(1) > li:nth-of-type(2) > nav > button"
+);
+const zoekNav = document.querySelector(
+  "nav > ul:nth-of-type(1) > li:nth-of-type(2) > nav"
+);
+const sluitZoek = document.querySelector(
+  "nav > ul:nth-of-type(1) > li:nth-of-type(2) > nav > section > button"
+);
+const winkelButton = document.querySelector(
+  "nav > ul:nth-of-type(1) > li:nth-of-type(1) > section > button:nth-of-type(1)"
+);
+const sluitWinkel = document.querySelector(
+  "nav > ul:nth-of-type(1) > li:nth-of-type(1) > section > section > button > img"
+);
+const winkelNav = document.querySelector(
+  "nav > ul:nth-of-type(1) > li:nth-of-type(1) > section"
+);
+const rechtsButton = document.querySelector(
+  "main > section:nth-of-type(2) > section > button:nth-of-type(2)"
+);
+const linksButton = document.querySelector(
+  "main > section:nth-of-type(2) > section > button:nth-of-type(1)"
+);
+const artikelList = document.querySelector(
+  "main > section:nth-of-type(2) > section > ul"
+);
+const hamburgerButton = document.querySelector(
+  "nav > ul > li:nth-of-type(3) > nav > button"
+);
 
 zoekButton.onclick = aanZoek;
 sluitZoek.onclick = uitZoek;
@@ -22,40 +44,36 @@ rechtsButton.onclick = gaVolgende;
 linksButton.onclick = gaTerug;
 hamburgerButton.onclick = hamburgerKlik;
 
-
 function aanZoek() {
-    zoekNav.classList.add("toonZoek");
+  zoekNav.classList.add("toonZoek");
 }
 
 function uitZoek() {
-    zoekNav.classList.remove("toonZoek");
+  zoekNav.classList.remove("toonZoek");
 }
 
 function aanWinkel() {
-    winkelNav.classList.add("toonWinkel");
+  winkelNav.classList.add("toonWinkel");
 }
 
 function uitWinkel() {
-    winkelNav.classList.remove("toonWinkel");
+  winkelNav.classList.remove("toonWinkel");
 }
 
 function gaVolgende() {
-    artikelList.scrollLeft = artikelList.scrollLeft + artikelList.offsetWidth;
+  artikelList.scrollLeft = artikelList.scrollLeft + artikelList.offsetWidth;
 }
 
 function gaTerug() {
-    artikelList.scrollLeft = artikelList.scrollLeft - artikelList.offsetWidth;
+  artikelList.scrollLeft = artikelList.scrollLeft - artikelList.offsetWidth;
 }
 
 function hamburgerKlik() {
-    if (hamburgerButton.ariaExpanded == "false") {
-        hamburgerButton.ariaExpanded = "true"
-    }
-    else {
-        hamburgerButton.ariaExpanded = "false"
-    }
-    
-    deNav.classList.toggle("toonMenu");
+  if (hamburgerButton.ariaExpanded == "false") {
+    hamburgerButton.ariaExpanded = "true";
+  } else {
+    hamburgerButton.ariaExpanded = "false";
+  }
+
+  deNav.classList.toggle("toonMenu");
 }
-
-
